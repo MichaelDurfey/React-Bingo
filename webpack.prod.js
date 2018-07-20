f7630c109bc7c742145e4b2a43b21267c0bd4777
Michael Durfey <mdurfey@me.com>
2018-07-20 11:18:16 -0700

    chore(webpack): add MiniCSSExtractPlugin for production CSS files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,22 @@
 import webpack from 'webpack';
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  }
   plugins: [
     new UglifyJSPlugin(),
+    new MiniCssExtractPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
