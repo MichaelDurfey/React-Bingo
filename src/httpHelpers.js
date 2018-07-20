e1e3a0a9821bb9e8ee61cfbdfc276ccc1a40b2ee
Michael Durfey <mdurfey@me.com>
2018-07-20 11:20:22 -0700

    style(Board, GameMaster): basic styling

--- src/httpHelpers.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const gameStart = () => axios.get('/start');
-
-const verifyWinner = id => axios.get(`/verify/${id}`);
-
-const drawBall = () => axios.get('/draw');
-
-export { gameStart, verifyWinner, drawBall };
