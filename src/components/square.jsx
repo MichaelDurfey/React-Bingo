fea6880d65c25fc8d4fc646757c4b76ea8df5381
Michael Durfey <mdurfey@me.com>
2018-07-20 20:23:23 -0700

    refactor(Square): removed shouldComponentUpdate method

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,27 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../styles/Square.css';
 
-class Square extends Component {
-  shouldComponentUpdate() {
-    const { selected } = this.props;
-    if (selected === true) {
-      return false;
-    }
-    return true;
-  }
-
-  render() {
-    const { number, selected } = this.props;
-    return (
-      <div
-        role="presentation"
-        className={selected ? `${styles.squareSelected} ${styles.square}` : styles.square}
-      >
-        {number < 10 ? `0${number}` : `${number}`}
-      </div>
-    );
-  }
+const Square = (props) => {
+  const { number, selected } = props;
+  return (
+    <div
+      role="presentation"
+      className={selected ? `${styles.squareSelected} ${styles.square}` : styles.square}
+    >
+      {number < 10 ? `0${number}` : `${number}`}
+    </div>
+  );
 };
 
 export default Square;
