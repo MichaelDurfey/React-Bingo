e7be3ea4643d2d82c9ee02461f643e466d194892
Michael Durfey <mdurfey@me.com>
2018-07-24 12:35:06 -0700

    perf(database): added helper functions for async handling of database requests

--- db/dbController.js
+++ db/dbController.js
@@ -1,14 +1,46 @@
 const db = require('./index');
 
-async function drawBall(req, res, ws) {
+function broadcast(data, ws, wss, WebSocket) {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN && client !== ws) {
+      client.send(JSON.stringify(data));
+    }
+  })
+};
+
+async function initialize(req, res) {
+  await db.initialize();
+  const boards = db.boards;
+  if (boards) {
+    return res.send(boards);
+  }
+    return res.sendStatus(500);
+};
+
+async function drawBall(req, res, ws, wss, WebSocket) {
   const ball = await db.drawBall();
   if (ball) {
-    console.log(ball)
-    ws.send(JSON.stringify(ball))
-    return res.send(ball)
-  } else {
-    return res.status(500).send()
+    if (ws.readyState === WebSocket.OPEN ) {
+      ws.send(JSON.stringify(ball));
+      broadcast(ball, ws, wss, WebSocket);
+    }
+    return res.send(ball);
   }
-}
+    return res.sendStatus(500);
+};
 
-module.exports = { drawBall }
+async function checkWin(req, res, ws, wss, WebSocket) {
+  const player = req.params.id;
+  const winner = await db.checkWin(player);
+  if (winner !== undefined) {
+    const response = { player, winner };
+    if (winner === true && ws.readyState === WebSocket.OPEN ) {
+      ws.send(JSON.stringify(response));
+      broadcast(response, ws, wss, WebSocket);
+    }
+   return res.send(response);
+  } 
+  return res.sendStatus(500);
+};
+
+module.exports = { drawBall, checkWin, initialize, broadcast }
